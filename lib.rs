3392c8a20f5beb5cc31d055285d7065202f339b3
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-03-01 16:12:38 +0300

    Contract instance with payee update

--- lib.rs
+++ lib.rs
@@ -2,68 +2,53 @@
 
 #[ink::contract]
 mod freeden_blogr {
+    use ink::storage::Mapping;
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct Payees{
+        account: AccountId,
+        percentages: u128,
+    }
+
     #[ink(storage)]
     pub struct FreedenBlogr {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        payees: Mapping<AccountId, u128>,
+        payout_amount: u128,
     }
-
+// Instantiate the contract with a default account and percentage
     impl FreedenBlogr {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(init_value: u128, account: AccountId) -> Self {
+            let mut accounts = Mapping::default();
+            let balance: Balance = init_value;
+            // let caller = Self::env().caller();
+            accounts.insert(&account, &balance);
+
+            Self {
+                payees: accounts,
+                payout_amount: balance,
+            }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
+// Adds a payee with a percentage share to the contract 
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn add_payee(&mut self, payee:AccountId, percentage: u128){
+            self.payees.insert(payee, &percentage);
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let freeden_blogr = FreedenBlogr::default();
-            assert_eq!(freeden_blogr.get(), false);
+        pub fn pay_accounts(&mut self) {
+            //distribute to accounts, the value based on the percentages
         }
 
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut freeden_blogr = FreedenBlogr::new(false);
-            assert_eq!(freeden_blogr.get(), false);
-            freeden_blogr.flip();
-            assert_eq!(freeden_blogr.get(), true);
+        #[ink(message)]
+        pub fn check_balance(&self, account: AccountId) -> u128 {
+            // let caller = Self::env().caller();
+            self.payees.get(&account).unwrap_or_default()
         }
     }
 }
