0aa8f205bf88b209411428495ad83b09d684ee57
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-03-01 18:33:53 +0300

    Add custom struct for payment values and update payout function

--- lib.rs
+++ lib.rs
@@ -3,21 +3,33 @@
 #[ink::contract]
 mod freeden_blogr {
     use ink::storage::Mapping;
+    use ink::prelude::vec::Vec;
 
 
+    #[derive(scale::Decode, scale::Encode)]
     #[cfg_attr(
         feature = "std",
         derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
     )]
     pub struct Payees{
-        account: AccountId,
         percentages: u128,
+        amount: u128,
+    }
+
+    impl Payees {
+        pub fn new() -> Self {
+            Self {
+                percentages: 0,
+                amount: 0,
+            }
+        }
     }
 
     #[ink(storage)]
     pub struct FreedenBlogr {
-        payees: Mapping<AccountId, u128>,
+        payees: Mapping<AccountId, Payees>,
         payout_amount: u128,
+        account_keys: Vec<AccountId>,
     }
 // Instantiate the contract with a default account and percentage
     impl FreedenBlogr {
@@ -25,16 +37,20 @@ mod freeden_blogr {
         pub fn new() -> Self {
             let accounts = Mapping::default();
             let balance: Balance = Balance::default();
+            let keys: Vec<AccountId> = Vec::new();
             Self {
                 payees: accounts,
                 payout_amount: balance,
+                account_keys: keys
             }
         }
 
 // Adds a payee with a percentage share to the contract 
         #[ink(message)]
-        pub fn add_payee(&mut self, payee:AccountId, percentage: u128){
-            self.payees.insert(payee, &percentage);
+        pub fn add_payee(&mut self, acc:AccountId, percentage: u128){
+            let mut payee: Payees = Payees::new();
+            payee.percentages = percentage; 
+            self.payees.insert(acc, &payee);
         }
 
         //update pauout amount on new subscription
@@ -44,14 +60,19 @@ mod freeden_blogr {
         }
 
         #[ink(message)]
-        pub fn pay_accounts(&mut self) {
+        pub fn pay_accounts(& self) {
             //distribute to accounts, the value based on the percentages
+            for key in &self.account_keys {
+                let mut acc = self.payees.get(key).unwrap();
+                acc.amount = acc.percentages * self.payout_amount
+            }
+
         }
         
         #[ink(message)]
         pub fn payee_balance(&self, account: AccountId) -> u128 {
             // let caller = Self::env().caller();
-            self.payees.get(&account).unwrap_or_default()
+            self.payees.get(account).unwrap().percentages
         }
 
 
