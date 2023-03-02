49f2969ad3910cb42c4574ea563a60b4617bb8f2
Kelly Muhindi <kmuhindi@protonmail.ch>
2023-03-02 11:02:47 +0300

    Rename variable and add account keys to mapper vector

--- lib.rs
+++ lib.rs
@@ -2,16 +2,15 @@
 
 #[ink::contract]
 mod freeden_blogr {
-    use ink::storage::Mapping;
     use ink::prelude::vec::Vec;
-
+    use ink::storage::Mapping;
 
     #[derive(scale::Decode, scale::Encode)]
     #[cfg_attr(
         feature = "std",
         derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
     )]
-    pub struct PayoutConfig{
+    pub struct PayoutConfig {
         percentages: u128,
         amount: u128,
     }
@@ -27,59 +26,58 @@ mod freeden_blogr {
 
     #[ink(storage)]
     pub struct FreedenBlogr {
-        payees: Mapping<AccountId, PayoutConfig>,
-        payout_amount: u128,
+        accounts: Mapping<AccountId, PayoutConfig>,
+        subscription_total: u128,
         account_keys: Vec<AccountId>,
     }
-// Instantiate the contract with a default account and percentage
+    // Instantiate the contract with a default account and percentage
     impl FreedenBlogr {
         #[ink(constructor)]
         pub fn new() -> Self {
-            let accounts = Mapping::default();
+            let init_accounts = Mapping::default();
             let balance: Balance = Balance::default();
             let keys: Vec<AccountId> = Vec::new();
             Self {
-                payees: accounts,
-                payout_amount: balance,
-                account_keys: keys
+                accounts: init_accounts,
+                subscription_total: balance,
+                account_keys: keys,
             }
         }
 
-// Adds a payee with a percentage share to the contract 
+        // Adds a payee with a percentage share to the contract
         #[ink(message)]
-        pub fn add_payee(&mut self, acc:AccountId, percentage: u128){
+        pub fn add_payee(&mut self, acc: AccountId, percentage: u128) {
             let mut payee: PayoutConfig = PayoutConfig::new();
-            payee.percentages = percentage; 
-            self.payees.insert(acc, &payee);
+            payee.percentages = percentage;
+            self.accounts.insert(acc, &payee);
+            //Update the keys mapper to add a key for this new payee
+            self.account_keys.push(acc);
         }
 
         //update pauout amount on new subscription
         #[ink(message)]
-        pub fn add_aubscriber_amount(&mut self, amount: u128){
-            self.payout_amount += amount;
+        pub fn add_aubscriber_amount(&mut self, amount: u128) {
+            self.subscription_total += amount;
         }
 
         #[ink(message)]
-        pub fn pay_accounts(& self) {
+        pub fn pay_accounts(&self) {
             //distribute to accounts, the value based on the percentages
             for key in &self.account_keys {
-                let mut acc = self.payees.get(key).unwrap();
-                acc.amount = acc.percentages * self.payout_amount
+                let mut acc = self.accounts.get(key).unwrap();
+                acc.amount = acc.percentages * self.subscription_total
             }
-            
-
         }
-        
+
         #[ink(message)]
         pub fn payee_balance(&self, account: AccountId) -> u128 {
             // let caller = Self::env().caller();
-            self.payees.get(account).unwrap().percentages
+            self.accounts.get(account).unwrap().percentages
         }
-
 
         #[ink(message)]
         pub fn total_balance(&self) -> u128 {
-            self.payout_amount
+            self.subscription_total
         }
     }
 }
